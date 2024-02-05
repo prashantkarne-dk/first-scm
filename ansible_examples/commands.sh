f946ebfbc2723406eda82e0663fb6887f9316a77
student <pk@koenig>
2024-02-05 14:04:27 +0530

    .

--- ansible_examples/commands.sh
+++ ansible_examples/commands.sh
@@ -2,4 +2,211 @@ https://github.com/haidaraM/ansible-playbook-grapher
 sudo dnf install graphviz python3.11 python3.11-pip -y
 python3.11 -m pip install ansible-navigator --user
 python3.11 -m pip install ansible-playbook-grapher --user
-ansible-playbook-grapher playbook.yaml
+ansible-playbook-grapher playbook.yaml    1  su -
+    2  sudo -i
+    3  history
+    4  su -
+    5  sudp -i
+    6  sudo -i
+    7  ansible-inventory -i inventory --graph
+    8  ansible-inventory -i inventory --graph europe
+    9  ansible --version
+   10  ansible-inventory --graph
+   11  ansible-config view
+   12  more /etc/hosts 
+   13  vi /etc/hosts 
+   14  gedit
+   15  gedit /etc/hosts
+   16  sudo gedit /etc/hosts
+   17  ssh-keygen
+   18  ssh-copy-id studdent@node1
+   19  ssh-copy-id student@node1
+   20  ssh-copy-id student@node2
+   21  ssh node1
+   22  ansible-config view
+   23  ansible-inventory -graph
+   24  ansible-inventory --graph
+   25  ssh node2
+   26  ansible europe,node2 -m ping
+   27  ansible europe,node2 -m reboot
+   28  ssh node1
+   29  ansible europe,node2 -m reboot
+   30  ansible europe,node2 -m ping
+   31  ansible europe,node2 -m reboot
+   32  ansible-doc -l
+   33  jkjkjjjls
+   34  ls
+   35  ls -ltr
+   36  pwd
+   37  ls
+   38  ls -ltr
+   39  pwd
+   40  exit
+   41  color theme
+   42  exit
+   43  pwd
+   44  ansible europe,node2 -m reboot
+   45  ansible europe,node2 -m ping
+   46  ansible europe,node2 -m pwd
+   47  ansible-navigator config view -m stdout
+   48  ssh node1
+   49  ssh node2
+   50  echo /usr/bin/bash
+   51  ansible-navigator lint --fix playbook.yaml
+   52  echo /usr/bin/bash
+   53  ansible-navigator lint --fix playbook.yaml
+   54  ansible-navigator run --syntax-check playbook.yaml
+   55  ansible-navigator run --syntax-check playbook.yaml -m standout
+   56  ansible-navigator run --syntax-check playbook.yaml -m stdout
+   57  ansible-navigator doc -l -m stdout
+   58  ansible-playbook playbook.yamlll
+   59  ansible-playbook playbook.yaml
+   60  ssh  node1
+   61  ansible centos -m reboot
+   62  pwd
+   63  cd .ssh
+   64  cd
+   65  cd .ssh
+   66  ls
+   67  rm *
+   68  cd ..
+   69  ssh node1
+   70  ssh node2
+   71  cd .ssh
+   72  ls
+   73  rm *
+   74  ls
+   75  cd
+   76  ssh-keygen
+   77  ssh-copy-id student@node1
+   78  ssh-copy-id student@node2
+   79  ssh node1
+   80  ansible-playbook playbook.yaml
+   81  pwd
+   82  cd ansible_examples/
+   83  ansible-playbook playbook.yaml
+   84  ansible-navigator run playbook.yaml -m stdout
+   85  ssh node1
+   86  ansible-playbook playbook.yaml
+   87  ansible-navigator
+   88  sudo yum install code
+   89  code
+   90  firefox
+   91  sudo dnf install python3-pip
+   92  python3 -m pip install ansible-navigator --user
+   93  echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.profile
+   94  source ~/.profile
+   95  ansible-navigator
+   96  man podman
+   97  ansible --version
+   98  cd ansible
+   99  cd /home/student/.ansible/
+  100  ls
+  101  cd
+  102  cd .local
+  103  cd ans
+  104  ls
+  105  cd bin
+  106  cd ans
+  107  ls
+  108  cd ansible
+  109  cd
+  110  clear
+  111  mkdir ansible_examples
+  112  code
+  113  ansible-navgator
+  114  ansible-navigator
+  115  pwd
+  116  cd ansible_examples/
+  117  ansible-navigator
+  118  ansible-navigator run playbook.yaml
+  119  ls
+  120  ansible-navigator replay playbook-artifact-2024-02-01T09:50:34.954793+00:00.json
+  121  ansible-navigator images
+  122  pwd
+  123  ansible-navigator run playbook.yaml --check -m stdout
+  124  ansible-navigator config view -m stdout
+  125  ansible-navigator inventory --graph -m stdout
+  126  ansible-navigator run playbook.yaml -m stdout
+  127  ansible-navigator run playbook.yaml --syntax-check
+  128  ansible-navigator run playbook.yaml --check
+  129  ansible-navigator collections
+  130  ansible-navigator doc -l -m stdout
+  131  ansible-navigator doc ansible.builtin.ping -m stdout
+  132  ode
+  133  code
+  134  sudo dnf install python3.11 python3.11-pip -y
+  135  python3.11 -m pip install ansible-navigator --user
+  136  ansible --version
+  137  ansible-navigator --version
+  138  python3.11 -m pip install --upgrade ansible-navigator --user
+  139  cd ansible_variables/
+  140  ansible-playbook playbook.yaml
+  141  ansible-navigator run playbook.yaml 
+  142  cd ../ansible_variables_fromFiles/
+  143  ansible-navigator lint --fix playbook.yaml
+  144  ansible-navigator run --syntax-check playbook.yaml -m stdout
+  145  ansible-navigator run --syntax-check playbook.yaml
+  146  ansible-navigator run playbook.yaml 
+  147  ansible-navigator run playbook.yaml -m stdout
+  148  ansible-navigator settings -effective -m stdout > abc.yml
+  149  ansible-navigator run playbook.yaml 
+  150  ssh node1
+  151  ansible-navigator run playbook.yaml -m stdout
+  152  cd ../ansible_inventory_variables/
+  153  ansible-navigator run playbook.yaml -m stdout
+  154  ssh node2
+  155  ssh node1
+  156  ansible-lint playbook.yaml --fix
+  157  ansible-navigator run playbook.yaml --syntax-check -m stdout
+  158  cd ../ansible_facts/
+  159  ansible-navigator run playbook.yaml 
+  160  ansible-playbook playbook.yaml
+  161  echo /usr/bin/bash
+  162  code
+  163  pwd
+  164  ls
+  165  ls -ltr
+  166  git clone https://github.com/prashantkarne-dk/first-scm.git
+  167  pwd
+  168  ls
+  169  ls -ltr
+  170  mv ansible_examples first-scm/
+  171  ls
+  172  cd first-scm/
+  173  ls
+  174  ls -ltr
+  175  git push
+  176  git remote set-url origin https://prashantkarne-dk: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@github.com/prashantkarne-dk/first-scm.git
+  177  git push origin https://prashantkarne-dk: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@github.com/prashantkarne-dk/first-scm.git
+  178  git push
+  179  pwd
+  180  ls
+  181  git config --global credential.helper '!f() { sleep 1; echo "username=git token=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"; }; f'
+  182  git pull
+  183  ls
+  184  get push
+  185  git push
+  186  git commit .
+  187  git commit -m "."
+  188  it config --global user.email "pk@koenig"
+  189  git config --global user.email "pk@koenig"
+  190  git commit -m "."
+  191  git all ansible_examples/
+  192  git add ansible_examples/
+  193  git commit -m "."
+  194  git push
+  195  sudo dnf install graphviz -y
+  196  sudo dnf install graphviz python3.11 python3.11-pip -y
+  197  python3.11 -m pip install ansible-navigator --user
+  198  python3.11 -m pip install ansible-playbook-grapher --user
+  199  ansible-playbook-grapher playbook.yaml
+  200  git add .
+  201  git commit -m "."
+  202  git push
+  203  git pull
+  204  ls
+  205  cd ansible_examples/
+  206  ls
+  207  ls -ltr
+  208  history >> commands.sh 
